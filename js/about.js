b926bdd2ca3cbb0969d0c6b4aba94e07e65aa593
sergey-uhanov <uhanov.sergio@gmail.com>
2025-09-03 21:00:27 +0300

    Updates

--- js/about.js
+++ js/about.js
@@ -1,1 +1,1 @@
-import{b as a,a as e,v as o,c as t}from"../assets/pop-up-request-call-C5dJJ6qo.js";document.addEventListener("DOMContentLoaded",()=>{a(),e(),o(),t()});
+import{b as a,a as e,v as o,c as t}from"../assets/pop-up-request-call-48VS0mA9.js";document.addEventListener("DOMContentLoaded",()=>{a(),e(),o(),t()});
